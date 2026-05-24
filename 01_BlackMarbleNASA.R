0586f7ffa44b61919b95d4cc14d19423ee5b6df1
Rafael Castillo <castillo.rafa@gmail.com>
2026-05-24 15:51:47 -0600

    Set goals of the project and authorship

---
+++ 01_BlackMarbleNASA.R
@@ -0,0 +1,12 @@
+################################################
+#                                              #
+#      CUBA'S ENERGY CRISIS OVER THE YEARS     #
+#                                              #   
+#   Bulk download night imagery from Nasa's    #
+#   Black Marble database and overlays it      #
+#   with geographic boundaries provided by     #
+#   Open Sources                               #
+#                                              #    
+#   By: RC Quintero                            #  
+#                                              #
+##############################################
